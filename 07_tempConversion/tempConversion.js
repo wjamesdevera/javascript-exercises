2f00efecb0e210c1ca2b3c56438ff81c1dcbfbcc
Winfrey De Vera <w.jamesdevera@gmail.com>
2023-04-12 11:09:32 +0800

    finished tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  return Math.round(((farenheit - 32) / 1.8) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return Math.round(((celsius * 1.8) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
