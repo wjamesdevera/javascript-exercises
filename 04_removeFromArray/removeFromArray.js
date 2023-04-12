d373235a5b77a4121d3cf67bfb50b4c1ccf2d51d
Winfrey De Vera <w.jamesdevera@gmail.com>
2023-04-12 10:20:37 +0800

    finished activity 1 - 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(a, b) {
+    const arr = Array.from(arguments);
+    for (let i = 1; i < arr.length; i++) {
+        let index = a.indexOf(arr[i]);
+        if (index > -1) {
+            a.splice(index, 1);
+        }
+    }
+    return a;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
