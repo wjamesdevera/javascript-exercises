651442343d7f04f58fa42056ab623bfd7e5571b3
Winfrey De Vera <w.jamesdevera@gmail.com>
2023-04-29 21:38:34 +0800

    finished calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,28 @@
-const add = function() {
-	
+const add = (a, b) => {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = (a, b) => {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((sum, item) => sum + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = (arr) => {
+  return arr.reduce((quotient, multiples) => quotient * multiples)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let quotient = a
+  for (let i = 0; i < b - 1; i++) {
+    quotient *= a;
+  }
+  return quotient;
 };
 
-const factorial = function() {
-	
-};
+const factorial = (n) => { return n > 0 ? n * factorial(n - 1) : 1;};
 
 // Do not edit below this line
 module.exports = {
