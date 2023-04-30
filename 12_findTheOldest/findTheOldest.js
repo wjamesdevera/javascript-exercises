d2b94b954010bef0cae106c842d6b2fadc84d704
Winfrey De Vera <w.jamesdevera@gmail.com>
2023-04-30 16:08:07 +0800

    finished 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = (people) => 
+    people.reduce((oldest, person) => {
+            const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+            const personAge = getAge(person.yearOfBirth, person.yearOfDeath);  
+            return oldestAge < personAge ? person : oldest;
+        });
 
-};
+function getAge(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
+/**
+ * finds the oldest person
+ * 
+ * finds the oldest person if someone is still living
+ * 
+ * finds the oldes person if the OLDEST is still living
+ * 
+ */
 
 // Do not edit below this line
 module.exports = findTheOldest;
