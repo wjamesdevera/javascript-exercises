e97e757626034093310568a4ab9146973b47061c
Winfrey De Vera <w.jamesdevera@gmail.com>
2023-04-30 14:45:08 +0800

    finished 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = (s) => {
+    const regex = /[\W_]/g;
+    const lowRegStr = s.toLowerCase().replace(regex, '');
+    const reverseStr = lowRegStr.split('').reverse().join('');
+    return reverseStr === lowRegStr;
 };
 
 // Do not edit below this line
